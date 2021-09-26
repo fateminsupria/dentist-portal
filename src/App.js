397734de88b8f33ce7217838e4c4806149a35526
Fatemin Supria <fateminsupria.me2016@gmail.com>
2021-09-26 21:28:42 +0600

    add route home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React  from 'react';
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './components/Home/Home/Home';
 function App() {
   return (
-    <div >
-    
-    </div>
+    <Router >
+    <Switch>
+      <Route exact path="/">
+        <Home></Home>
+      </Route>
+    </Switch>
+    </Router>
   );
 }
 
