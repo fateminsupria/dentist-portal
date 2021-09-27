d96f9eeffa21760fe61b99a2244a82acb2f3fbdb
Fatemin Supria <fateminsupria.me2016@gmail.com>
2021-09-27 23:31:43 +0600

    change button

--- src/components/Home/HeaderMain/HeaderMain.js
+++ src/components/Home/HeaderMain/HeaderMain.js
@@ -5,9 +5,11 @@ const HeaderMain = () => {
     return (
         <main className="row">
           <div className = "col-md-4 offset-md-1">
-              <h1> Your smile :) Our Priority</h1>
-              <p>lorem  something</p>
-              <button class="btn btn-primary">Appointment</button>
+              <h1> Your smile :) 
+                  <br/>
+                  Our Priority</h1>
+              <p>lorem  something lorem  something lorem  something something</p>
+              <button class="btn btn-dark"><b>Appointment</b></button>
               </div>  
          <div className="col-md-6">
              <img src={chair} alt="" className="img-fluid"/>
